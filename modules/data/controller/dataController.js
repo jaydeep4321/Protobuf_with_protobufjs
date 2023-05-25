ded04730841c381fcd90e6ed69e8fb35a77712eb
jaydeep4321 <jaydeepvaghela.thetatechnolabs@gmail.com>
2023-05-25 08:48:02 +0530

    updated from home

--- modules/data/controller/dataController.js
+++ modules/data/controller/dataController.js
@@ -1,40 +1,56 @@
-const { Data, AllData } = require("../../../data_pb");
+// const { Data, AllData } = require("../../../data_pb");
 const dataModel = require("../model/dataModel");
 const protobuf = require("protobufjs");
+
+let dataMessage;
+let allDataMessage;
+
+async function loadProtobufJs() {
+  try {
+    const root = await protobuf.load("data.proto");
+    dataMessage = root.lookupType("Data");
+    allDataMessage = root.lookupType("AllData");
+    console.log("=====ProtobufJs loaded successfully=====");
+  } catch (error) {
+    console.error("====Error loading ProtobufJs=====", error);
+  }
+}
+
+loadProtobufJs();
 
 exports.getAllData = async (req, res, next) => {
   try {
     let documents = await dataModel.find();
 
     const dataMessages = documents.map((document) => {
-      const data = new Data();
-      data._id = document._id.toString();
-      data.name = document.name;
-      data.age = document.age;
-      data.description = document.description;
+      const data = {
+        _id: document._id.toString(),
+        name: document.name,
+        age: document.age,
+        description: document.description,
+      };
 
       return data;
     });
 
-    console.log("dataMessages ==>", dataMessages);
-
-    const dataRoot = await protobuf.load("data.proto");
-    const dataMessage = dataRoot.lookupType("Data");
-    const allDataMessage = dataRoot.lookupType("AllData");
+    // console.log("dataMessages ==>", dataMessages);
 
     const payload = {
       allData: dataMessages.map((data) => dataMessage.create(data)),
     };
 
-    // console.log("allData ==>", payload);
+    // let errMsg = allDataMessage.verify(payload);
+    // if (errMsg) {
+    //   throw new Error("Payload verification failed: " + errMsg);
+    // }
 
     let encodedData = allDataMessage.encode(payload).finish();
 
     console.log("Encoded Data ==>", encodedData);
-    // const encodedDataBase64 = encodedData.toString("base64");
-    encodedData = new Uint8Array(encodedData);
+    // encodedData = new Uint8Array(encodedData);
 
-    // console.log("Unit8Array ==>", encodedData);
+    const decodedData = allDataMessage.decode(encodedData)
+    console.log(decodedData)
 
     res.status(200).send(encodedData);
   } catch (error) {
@@ -53,3 +69,4 @@ exports.findAll = async (req, res, next) => {
     res.status(500).json({ message: "An error occurred while fetching data" });
   }
 };
+
